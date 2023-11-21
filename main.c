58dddd691053918b5d72ef2fd974edacc1f4506b
Vitor Souza <desenvolvedor.vitorsouza@gmail.com>
2023-11-21 00:54:48 -0400

    feat: added a function that log in the user, and a two other functions that help it

--- main.c
+++ main.c
@@ -13,30 +13,60 @@
 #define sleep(x) Sleep(1000 * (x))
 #endif
 
-struct usuario { char *senha; signed long id; char *nome; };
+struct usuario { char *nome; char *senha; };
 
 void telaInicial(), telaCadastroFuncionario(), telaLoginFuncionario();
-char usu[50];
-char senhaString[10];
-float GeralResiduo, GeralValor, GeralCusto;
-int norte, nordeste, sul, sudeste, centroOeste;
+int conectarFuncionario(char *nome, char *senha), salvarFuncionario(struct usuario u);
+struct usuario fabricarUsuario(char *nome, char *senha);
 
-int main(void){
-	setlocale(LC_ALL, "Portuguese");
-	telaInicial();
+
+int main(void) {
+	setlocale(LC_ALL, "Portuguese_Brazil");
+	conectarFuncionario("Maria", "matematica180");
 	return 0;
 }
 
-
 void escreverFrasePadrao(char *frase) {
 	printf("%s\n\n", frase);
+}
+
+int funcionarioExiste(char *nome, char *linha) {
+	char *token = strtok(linha, ";");
+	if (strcmp(nome, token) == 0) { return 1; }
+
+	return 0;
+}
+
+int verificarSenhaFuncionario(char *senha, char *linha) {
+	if (strstr(linha, senha)) {
+		return 1;
+	}
+
+	return 0;
+}
+
+int conectarFuncionario(char *nome, char *senha) {
+	FILE *arquivo = fopen("funcionarios.txt", "r");
+	char linha[128];
+
+	while (fscanf(arquivo, "%s\n", linha) > 0) {
+		char copia[128];
+		strcpy(copia, linha);
+
+		if (!funcionarioExiste(nome, copia)) { continue; }
+
+		if (verificarSenhaFuncionario(senha, linha)) { return 1; }
+	}
+	fclose(arquivo);
+
+	return 0;
 }
 
 int salvarFuncionario(struct usuario u) {
 	char *linha = malloc(sizeof(u));
 	FILE *arquivo = fopen("funcionarios.txt", "a");
 
-	sprintf(linha, "%s,%s,%ld;", u.nome, u.senha, u.id);
+	sprintf(linha, "%s;%s\n", u.nome, u.senha);
 	fprintf(arquivo, "%s", linha);
 	
 	free(linha);
@@ -49,13 +79,8 @@ struct usuario fabricarUsuario(char *nome, char *senha)
 {
 	struct usuario fabricado;
 
-	srand(time(0));
-
-	signed long id = rand() % 100000000000000000;
-
 	fabricado.nome = nome;
 	fabricado.senha = senha;
-	fabricado.id = id;
 
 	return fabricado;
 }
@@ -78,29 +103,29 @@ void escreverRoteiro(char *roteiro[], unsigned int tamanho)
 
 void telaInicial()
 {
-		char *roteiro[] = {"Olá funcionário, você já possui um login?", "Digite (1) para sim", "Digite (2) para não", "Digite (3) para fechar o programa"};
+	char *roteiro[] = {"Olá funcionário, você já possui um login?", "Digite (1) para sim", "Digite (2) para não", "Digite (3) para fechar o programa"};
 
-		size_t tamanhoRoteiro = sizeof(roteiro) / sizeof(roteiro[0]);
-    uint8_t escolha;
+	size_t tamanhoRoteiro = sizeof(roteiro) / sizeof(roteiro[0]);
+	uint8_t escolha;
 
-		escreverRoteiro(roteiro, tamanhoRoteiro);
-    scanf("%hhu", &escolha);
+	escreverRoteiro(roteiro, tamanhoRoteiro);
+	scanf("%hhu", &escolha);
 
-    switch(escolha){
-        case 1:
-            telaLoginFuncionario();
-            break;
-        case 2:
-            telaCadastroFuncionario();
-            break;
-        case 3:
-            printf("\nFechando o programa...\n");
-            exit(0);
-            break;
-        default:
-            printf("Resposta inválida, tente novamente!");
-            break;
-    }
+	switch(escolha) {
+		case 1:
+			telaLoginFuncionario();
+			break;
+		case 2:
+			telaCadastroFuncionario();
+			break;
+		case 3:
+			printf("\nFechando o programa...\n");
+			exit(0);
+			break;
+		default:
+			printf("Resposta inválida, tente novamente!");
+			break;
+	}
 }
 
 void telaRegistroSucesso()
@@ -118,9 +143,7 @@ void telaRegistroSucesso()
 
 void telaCadastroFuncionario(){
 	char *roteiro[] = {"Crie o seu usuário para o login no sistema."};
-
 	size_t tamanhoRoteiro = sizeof(roteiro) / sizeof(roteiro[0]);
-
 	escreverRoteiro(roteiro, tamanhoRoteiro);
 
 	char nome[32], senha[64];
